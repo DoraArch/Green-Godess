7cfd6cfc0f90acd752fe07cae3674c01aa782a0d
DoraArch <naphieran@gmail.com>
2023-07-20 16:14:31 +0300

    review

--- java.js
+++ java.js
@@ -1,14 +1,1 @@
-function cardspace(){
-    var carddigit = document.getElementById('cardno').Value;
-    if(carddigit.length ==4 || carddigit.length ==9 || carddigit.length ==14){
-        document.getElementById('cardno').Value = carddigit+"";
-        document.getElementById('cardno').max =1;
-    }
-}
-function addslashes(){
-    var expiredate = document.getElementById('validtill').Value;
-    if(expiredate.length ==2){
-        document.getElementById('validtill').Value = expiredate+"";
-        document.getElementById('validtill').max =1;
-    }
-}
+
